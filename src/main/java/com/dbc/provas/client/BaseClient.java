4934e5ada9093a313e9a76c51156e5d2111d180e
VictorPrudente <victorprudt@gmail.com>
2024-09-16 20:40:19 -0300

    test

--- src/main/java/com/dbc/provas/client/BaseClient.java
+++ src/main/java/com/dbc/provas/client/BaseClient.java
@@ -10,7 +10,7 @@ import static io.qameta.allure.Allure.step;
 public class BaseClient {
 
     final String BASE_URI = "https://provas-back-hml.onrender.com";
-
+    final String test = "test";
     public RequestSpecification set() {
 
         return step("Configurando a especificação base da requisição", () ->
