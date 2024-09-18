192dcc4df834c098b019a1e31f700dfb3c5731cc
Victor Prudente <143949574+VictorPrudente@users.noreply.github.com>
2024-09-18 20:49:59 -0300

    Update AllureReport.py

--- scripts/AllureReport.py
+++ scripts/AllureReport.py
@@ -5,59 +5,64 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import os
+import re
 
+# Configs base
 webhook_key = os.getenv('WEBHOOK_KEY')
 base_url = "https://teste-pipe-line.vercel.app/"
 
+# Opcoes para a print
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
-
 driver.get(base_url)
-porcentagem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'text[class="chart__caption"]')))
-porcentagem_text = porcentagem.text
 
-qtdTeste = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.splash__title')))
-qtdTeste_text = qtdTeste.text
+# Atributos para o Print do Discord
+porcentagem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'text[class="chart__caption"]'))).text
+qtdTeste = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.splash__title'))).text
+data = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.widget__title'))).text
+tempoDecorrido = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.widget__subtitle'))).text
 
-data = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.widget__title')))
-data_text = data.text
-
-tempoDecorrido = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.widget__subtitle')))
-tempoDecorrido_text = tempoDecorrido.text
-
+# Aqui se não tiver erro dá tudo certo :D
 try:
-    qtdFalhas = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div:nth-of-type(2) > div:nth-of-type(2) > div:nth-of-type(2) > div > div > a:nth-of-type(1) > div:nth-of-type(2) > div > div')))
-    qtdFalhas_text = qtdFalhas.text
+    qtdFalhas = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div:nth-of-type(2) > div:nth-of-type(2) > div:nth-of-type(2) > div > div > a:nth-of-type(1) > div:nth-of-type(2) > div > div'))).text
 except Exception as e:
-    qtdFalhas_text = "0"
+    qtdFalhas = "0"
 
+# Tira o print
 driver.save_screenshot("allure_screenshot.png")
 
+# Dá tchau pro browser
 driver.quit()
 
+# Configura o gancho do pirata
 webhook = DiscordWebhook(url=webhook_key)
 
 embed = DiscordEmbed(
-        title="Relatório Testes API - Provas",
-        description="[LINK VERCEL](https://estudos-github-actions.vercel.app)"
+        title="Relatório de testes API Provas",
+        description=f"[LINK VERCEL]({base_url})"
  )
 
+# Cor da borda da print
+embed.set_color("00FF00" if porcentagem_text == "100%" else "FF0000")
 
-if(porcentagem_text == "100%"):
-    embed.set_color("00FF00")
-else:
-    embed.set_color("FF0000")
+# Atributos da print
+embed.add_embed_field("Quantidade de teste", qtdTeste, False)
 
-embed.add_embed_field("Quantidade de teste", qtdTeste_text, False)
 if qtdFalhas_text != "0":
-    embed.add_embed_field("Falhas", qtdFalhas_text, False)
-embed.add_embed_field("Tempo decorrido", tempoDecorrido_text, False)
+    embed.add_embed_field("Falhas", qtdFalhas, False)
 
+tempo_decorrido_match = re.search(r"\((.*?)\)", tempoDecorrido)
+if tempo_decorrido_match:
+    embed.add_embed_field("Tempo decorrido", tempo_decorrido_match.group(1), False)
+
+# Anexa a fota
 with open("./allure_screenshot.png", "rb") as f:
     webhook.add_file(file=f.read(), filename="allure_screenshot.png")
 embed.set_image(url="attachment://allure_screenshot.png")
 
+# Adiciona o gancho
 webhook.add_embed(embed)
 
+# E vrau
 webhook.execute()
