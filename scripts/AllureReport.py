d2c3a451dd0de5d31f043d1675160419213c1257
Victor Prudente <143949574+VictorPrudente@users.noreply.github.com>
2024-09-19 12:19:38 -0300

    Update AllureReport.py

--- scripts/AllureReport.py
+++ scripts/AllureReport.py
@@ -1,40 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from discord_webhook import DiscordWebhook, DiscordEmbed
-import os
 import re
+import os
 
-# Configs base
-webhook_key = os.getenv('WEBHOOK_KEY')
-base_url = "https://teste-pipe-line.vercel.app/"
+webhook_key = os.getenv('DISCORD_WEBHOOK_URL')
+base_url = os.getenv('REPORT_URL')
+title = os.getenv('PIPELINE_NAME')
 
-# Opcoes para a print
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
+driver.set_window_size(1920,1080)
 driver.get(base_url)
 
-
-# Desabilitar animações
-driver.execute_script("""
-    var style = document.createElement('style');
-    style.innerHTML = '* { transition: none !important; animation: none !important; }';
-    document.head.appendChild(style);
-""")
-
-
-# Metodo para pegar os atributos
 def getElement(selector, timeout=10):
     try:
         return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector))).text
     except Exception:
         return None
 
-
-# Atributos para o Print do Discord
 porcentagem = getElement('text[class="chart__caption"]')
 qtdTeste = getElement('.splash__title')
 data = getElement('.widget__title')
@@ -42,25 +29,25 @@ tempoDecorrido = getElement('.widget__subtitle')
 qtdFalhas = getElement('div:nth-of-type(2) > div:nth-of-type(2) > div:nth-of-type(2) > div > div > a:nth-of-type(1) > '
                        'div:nth-of-type(2) > div > div') or "0"
 
-# Tira o print
+driver.execute_script("""
+    var style = document.createElement('style');
+    style.innerHTML = '* { transition: none !important; animation: none !important; }';
+    document.head.appendChild(style);
+""")
+
 driver.save_screenshot("allure_screenshot.png")
 
-# Dá tchau pro browser
 driver.quit()
 
-# Configura o gancho do pirata
 webhook = DiscordWebhook(url=webhook_key)
 
 embed = DiscordEmbed(
-    title="Relatório de testes API Provas"
+    title=title
 )
 
-embed.set_url(f"{base_url}")
-
-# Cor da borda da print
 embed.set_color("00FF00" if porcentagem == "100%" else "FF0000")
 
-# Atributos da print
+embed.set_url(f"{base_url}")
 embed.add_embed_field("Quantidade de teste", qtdTeste, False)
 
 if qtdFalhas != "0":
@@ -70,13 +57,10 @@ tempo_decorrido_match = re.search(r"\((.*?)\)", tempoDecorrido)
 if tempo_decorrido_match:
     embed.add_embed_field("Tempo decorrido", tempo_decorrido_match.group(1), False)
 
-# Anexa a fota
 with open("./allure_screenshot.png", "rb") as f:
     webhook.add_file(file=f.read(), filename="allure_screenshot.png")
 embed.set_image(url="attachment://allure_screenshot.png")
 
-# Adiciona o gancho
 webhook.add_embed(embed)
 
-# E vrau
 webhook.execute()
